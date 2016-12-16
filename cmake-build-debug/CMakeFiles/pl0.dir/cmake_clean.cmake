0d516cf79d7237d87ea8acfca59385618844974a
Peterson Xu <932446461@qq.com>
2016-12-16 09:24:59 +0800

    confilict

---
+++ cmake-build-debug/CMakeFiles/pl0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pl0.dir/pl0.c.o"
+  "pl0.pdb"
+  "pl0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pl0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pl0.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pl0.
+# This may be replaced when dependencies are built.
